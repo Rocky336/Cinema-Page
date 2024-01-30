774badf6394371228fe01c9a9070b4965483c9a5
Rocky336 <rockygiuby@gmail.com>
2024-01-30 13:30:04 +0100

    Updating both files

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const pages = document.getElementsByClassName("pages-container");
 const buttons = document.getElementsByTagName("button");
 const data = new Date();
+const oggi = ((data.getDay() + 6) % 7)
 
 async function setMovies() {
-    const response = await fetch('./data.json');
-    const dati = await response.json();
+    const response  =    await fetch('./data.json');
+    const dati      =    await response.json();
     for(let key in dati){
         for(let k = 0;k<dati[key].length;k++){
             pages.namedItem(key).innerHTML +='<div class="page">' +
             '<div><img src="https://picsum.photos/30'+(Number(key)+k)+'"><div>' +
-            '<strong>'+dati[key][k].title+'</strong>'+
+            '<strong>'      +dati[key][k].title +'</strong>'+
             '<span>Autore: '+dati[key][k].author+'</span>'+
-            '<span>Trama: '+dati[key][k].plot+'</span></div></div>'+
-            '<span>'+dati[key][k].timing+'</span></div>'
+            '<span>Trama: ' +dati[key][k].plot  +'</span></div></div>'+
+            '<span>'        +dati[key][k].timing+'</span></div>'
         }
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+    let giorni = new Date();
+    giorni.setDate(data.getDate() - oggi);
     for (let i = 0; i < buttons.length; i++) {
+        buttons[i].innerHTML+=" " + giorni.getDate()+"/"+(giorni.getMonth()+1);
+        giorni.setDate(giorni.getDate()+1);
         buttons[i].addEventListener("click", () => {
             for (let i = 0; i < pages.length; i++)
                 pages[i].style.display = "none";
@@ -26,5 +31,5 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
     setMovies();
-    pages.namedItem(((data.getDay() + 6) % 7)).style.display = "block";
+    pages.namedItem(oggi).style.display = "block";
 });
